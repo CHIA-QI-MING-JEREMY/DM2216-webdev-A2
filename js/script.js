cd65abd063c1caa35abed64889c0621d909b948a
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2022-08-03 02:14:57 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let acc = document.querySelectorAll(".accordion");
+    for (let i = 0; i < acc.length; i++)
+    {
+        acc[i].addEventListener("click", ()=>{
+            acc[i].classList.toggle("active");
+            let panel = acc[i].nextElementSibling;
+            if (panel.style.maxHeight)
+            {
+                panel.style.maxHeight = null;
+            }
+            else
+            {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
